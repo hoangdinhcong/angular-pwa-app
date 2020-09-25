75742f94c0ff93da4d7a23db4ecf808e541655ea
Hoang Dinh Cong <hodico.iam@gmail.com>
2020-09-25 23:10:43 +0700

    add interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { HttpConfigInterceptor } from './intreceptors/httpconfig.interceptor';
 
 @NgModule({
   declarations: [
@@ -11,9 +13,16 @@ import { environment } from '../environments/environment';
   ],
   imports: [
     BrowserModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: HttpConfigInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/intreceptors/httpconfig.interceptor.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+@Injectable()
+export class HttpConfigInterceptor implements HttpInterceptor {
+    constructor() { }
+
+    intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+        // const headers = request.headers.set('Content-Type', 'application/json');
+        // const authReq = request.clone({ headers });
+        // return next.handle(authReq);
+
+        const token: string = localStorage.getItem('token');
+
+        if (token) {
+            request = request.clone({ headers: request.headers.set('Authorization', 'Bearer ' + token) });
+        }
+
+        if (!request.headers.has('Content-Type')) {
+            request = request.clone({ headers: request.headers.set('Content-Type', 'application/json') });
+        }
+
+        request = request.clone({ headers: request.headers.set('Accept', 'application/json') });
+
+        return next.handle(request).pipe(
+            map((event: HttpEvent<any>) => {
+                if (event instanceof HttpResponse) {
+                    console.log('event--->>>', event);
+                }
+                return event;
+            }));
+
+    }
+}
